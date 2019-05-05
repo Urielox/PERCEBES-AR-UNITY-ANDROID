7da4d66ae83bd9eea3437679de89e9066a369a5f
Urielox <urielox2011@hotmail.com>
2019-05-05 01:04:59 -0500

    aqui esta la app ar

---
+++ Assets/fishcontroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class fishcontroller : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, y);
+
+        rb.velocity = movement * .5f;
+
+    }
+}
